4db260da82938dc0acdee0c4a498b3072b2eb559
ablakanovamin0-svg <ablakanovamin0@gmail.com>
2025-11-19 21:38:51 +0300

    Создание класса Tests и создание класса ExpessionSolver

---
+++ src/test/java/org/example/Tests.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Класс для тестирования функционала методов класса ExpressionSolver
+ * @author ablakanovamin0-svg
+ * @version 1.0
+ */
+public class Tests {
+
+    @Test
+    void testingSolverExpression() {
+
+    }
+
+}
